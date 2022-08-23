7789f2a353a8fecafe95567441c9c4212c24d57b
James Hibbeard <jhibbeard@gmail.com>
2022-08-23 11:28:47 +0200

    fixed issue with migration, renamed table to schema

--- app/models/time_travel.rb
+++ app/models/time_travel.rb
@@ -1,4 +1,4 @@
-class TimeMachine < ApplicationRecord
+class TimeTravel < ApplicationRecord
   belongs_to :user
   has_many :users
 
--- db/migrate/20220822161616_create_time_travels.rb
+++ db/migrate/20220822161616_create_time_travels.rb
@@ -1,10 +1,9 @@
 class CreateTimeMachines < ActiveRecord::Migration[7.0]
   def change
-    create_table :time_machines do |t|
+    create_table :time_travels do |t|
       t.string :name
       t.string :location
       t.string :period_of_time
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
