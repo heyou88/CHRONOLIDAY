20e1c65723e84617dbf5475a0bc97d75884bb8f7
James Hibbeard <jhibbeard@gmail.com>
2022-08-23 17:43:21 +0200

    Cleaned up bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,10 +3,6 @@ class BookingsController < ApplicationController
     @bookings = Booking.where(user: current_user)
   end
 
-  # def show
-  #   @booking = Booking.find(params[:id])
-  # end
-
   def new
     @booking = Booking.new
   end
@@ -16,16 +12,6 @@ class BookingsController < ApplicationController
     @booking.save
     redirect_to booking_path(@booking)
   end
-
-  # def edit
-  #   @booking = Booking.find(params[:id])
-  # end
-
-  # def update
-  #   @booking = Booking.find(params[:id])
-  #   @booking.update(booking.params)
-  #   redirect_to booking_path(@booking)
-  # end
 
   def destroy
     @booking =  Booking.find(params[:id])
@@ -38,5 +24,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
-
 end
