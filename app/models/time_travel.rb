3df86ac5c64fc8213cc16617a5a355248263a5d9
you he <heyou88@hotmail.com>
2022-08-23 12:33:09 +0200

    created model booking and relation to user and time_travel

--- app/models/time_travel.rb
+++ app/models/time_travel.rb
@@ -1,5 +1,6 @@
 class TimeTravel < ApplicationRecord
-
+  has_many :users, through: :booking
+  has_many bookings
   validates :location, presence: true, uniqueness: true
   validates :period_of_time, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_161616) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_101757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.boolean "availbility"
+    t.integer "price"
+    t.string "start_date"
+    t.string "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "time_travel_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["time_travel_id"], name: "index_bookings_on_time_travel_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "time_travels", force: :cascade do |t|
     t.string "name"
@@ -34,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_161616) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "time_travels"
+  add_foreign_key "bookings", "users"
 end
