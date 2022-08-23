06461db1c237b75089acc94f278b22178cf89c4b
you he <heyou88@hotmail.com>
2022-08-23 15:51:18 +0200

    setting up of bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,42 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking.params)
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking =  Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_others
+  end
+
+
+  private
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+end
