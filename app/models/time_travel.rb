30a3a235fc52d5c88db8a034b7f09faa0bc74e5e
you he <heyou88@hotmail.com>
2022-08-23 14:18:50 +0200

    time_travelmodified

--- app/models/time_travel.rb
+++ app/models/time_travel.rb
@@ -1,6 +1,5 @@
 class TimeTravel < ApplicationRecord
-  has_many :users, through: :booking
-  has_many bookings
+  has_many :bookings
   validates :location, presence: true, uniqueness: true
   validates :period_of_time, presence: true
 end
